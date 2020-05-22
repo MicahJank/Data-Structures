adf6fb5dfbf3b5d17eeca899565944f7064d8c01
MicahJank <MicahJank@users.noreply.github.com>
2020-05-21 22:50:51 -0500

    added the text buffer file

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -6,6 +6,27 @@ class ListNode:
     def insert_after(self, value):
         current_next = self.next
         self.next = ListNode(value, current_next)
+# How do you find and return the middle node of a singly linked 
+# list in one pass? You do not have access to the length of the list. 
+# If the list is even, you should return the first of the two "middle" nodes. You may not store the nodes in another data structure.
+
+'''
+   def find_middle(self):
+        middle = self.head
+        end = self.head
+        while end.next != None and end.next.next != None:
+            end = end.next.next
+            middle = middle.next
+        return middle
+'''
+
+
+# head should now be tail
+    # tail should now be head
+    # no recursion, no other data structures
+    # 
+    #  def reverse_list(self):
+    #     pass
 
 
 class LinkedList:
@@ -42,7 +63,7 @@ class LinkedList:
         value = self.head.value
         self.head = self.head.next
         self.length -= 1
-        print(self.tail.value)
+
         return value
 
             
