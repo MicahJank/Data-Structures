9c5867a6dd4839d2169e44d495dfcd7817b27230
Micah <micahjank@gmail.com>
2020-05-28 14:35:38 -0500

    finished with stretch completed

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,13 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+import sys
+# sys.path.append('../queue')
+from queue import Queue
+
+sys.path.append('../stack')
+from stack import Stack
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -134,40 +141,83 @@ class BSTNode:
 
     # Part 2 -----------------------
 
+
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if node.left is not None:
+            node.left.in_order_print(node.left)
+            print(node.value)
+        else:
+            print(node.value)
+        
+        if node.right is not None:
+            node.right.in_order_print(node.right)
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
         # make a queue
+        queue = Queue()
         # enqueue the node
+        queue.enqueue(node)
         # as long as the queue is not empty
+        while len(queue) > 0:
             # dequeue from the front of the queue, this is the current node
+            current_node = queue.dequeue()
             # check that the kids are not None
-            # enqueue the kids of the current node on the queue
+            if current_node.left is not None:
+                # enqueue the kids of the current node on the queue
+                queue.enqueue(current_node.left)
+
+            if current_node.right is not None:
+                queue.enqueue(current_node.right)
+
+            print(current_node.value)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
         # make a stack
+        stack = Stack()
         # push the node on the stack
+        stack.push(node)
         # as long as the stack is not empty
+        while len(stack) > 0:
             # pop off the stack, this is our current node
+            current_node = stack.pop()
+
             # check that the kids are not None
-            # put the kids of the current nod on the stack
+            if current_node.right is not None:
+                stack.push(current_node.right)
+
+            if current_node.left is not None:
+                # put the kids of the current node on the stack
+                stack.push(current_node.left)
+            
+            print(current_node.value)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
     def pre_order_dft(self, node):
-        pass
+        print(node.value)
+        if node.left is not None:
+            node.pre_order_dft(node.left)
+
+        if node.right is not None:
+            node.pre_order_dft(node.right)
+        
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
-        pass
+        if node.left is not None:
+            node.left.post_order_dft(node.left)
+        
+        if node.right is not None:
+            node.right.post_order_dft(node.right)
+            print(node.value)
+        else:
+            print(node.value)
