2e17a1ef6010fa6c2e81ae8db22d52b82a37b869
Micah <micahjank@gmail.com>
2020-05-29 17:58:17 -0500

    commented out unneeded class

--- queue/queue.py
+++ queue/queue.py
@@ -39,24 +39,24 @@ class Queue:
             return self.storage.pop()
         return
 
-DoublyLinkedList implementation
-class Queue:
-    def __init__(self):
-        self.size = 0
-        self.storage = DoublyLinkedList()
+# DoublyLinkedList implementation
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = DoublyLinkedList()
     
-    def __len__(self):
-        return self.size
+#     def __len__(self):
+#         return self.size
 
-    def enqueue(self, value):
-        self.storage.add_to_head(value)
-        self.size += 1
+#     def enqueue(self, value):
+#         self.storage.add_to_head(value)
+#         self.size += 1
 
-    def dequeue(self):
-        if self.size > 0:
-            self.size -= 1
-            return self.storage.remove_from_tail()
-        return
+#     def dequeue(self):
+#         if self.size > 0:
+#             self.size -= 1
+#             return self.storage.remove_from_tail()
+#         return
 
 
 # Stack Implementation
